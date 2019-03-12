dbb5eb53b7ac06518321f1e26ec9dd61bbb152ca
Tadas Ivanauskas <ivanauskas15@gmail.com>
2019-03-12 23:43:46 +0100

    Login and home activities

--- app/src/main/java/ivanauskas/tadas/heartmonitor/MainActivity.java
+++ app/src/main/java/ivanauskas/tadas/heartmonitor/MainActivity.java
@@ -1,13 +1,68 @@
 package ivanauskas.tadas.heartmonitor;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        item.setChecked(true);
+                        // close drawer when item is tapped
+                        drawerLayout.closeDrawers();
+                        switch (item.getItemId()){
+                            case R.id.nav_home:
+                                // TODO home
+                                Toast.makeText(MainActivity.this,"Not yet implemented", Toast.LENGTH_SHORT).show();
+                                return true;
+                            case R.id.nav_settings:
+                                // TODO settings page
+                                Toast.makeText(MainActivity.this,"Not yet implemented", Toast.LENGTH_SHORT).show();
+                                return true;
+                            case R.id.nav_logout:
+                                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                                return true;
+                        }
+                        return true;
+                    }
+                }
+        );
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
